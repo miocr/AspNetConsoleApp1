fdbe9bdaf1d28fccb2ebc27ed1c0aabcf604c0d8
miocr <omihula@hotmail.com>
2019-02-14 18:25:55 +0100

    x

---
+++ UnitTests1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspNetConsoleApp1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            TestContext.WriteLine("HELLO");
+            System.Console.WriteLine("TEST");
+            Model1 model1 = new Model1()
+            {
+                Id = 1,
+                Name = "Karel"
+            };
+
+            Assert.AreEqual("Karel", model1.Name);
+        }
+    }
+}
