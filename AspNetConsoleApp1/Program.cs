b853bb4653bbe3b15cd242ac38d3e921fdf993ca
miocr <omihula@post.cz>
2019-02-14 18:03:57 +0100

    Initial check-in of module AspNetConsoleApp1

---
+++ AspNetConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AspNetConsoleApp1
+{
+
+    public class Model1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
