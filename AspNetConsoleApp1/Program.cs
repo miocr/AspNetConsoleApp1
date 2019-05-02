bebc6a1ef838fedc0c59ad2ae0cd316ef8511b1e
miocr <omihula@hotmail.com>
2019-05-02 22:25:12 +0200

    Mac

--- AspNetConsoleApp1/Program.cs
+++ AspNetConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Configuration;
+using System.Runtime;
+
 
 namespace AspNetConsoleApp1
 {
@@ -14,6 +17,33 @@ namespace AspNetConsoleApp1
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string keyName = "Counter";
+            string keyNameDate = "Date";
+            string keyNameTicks = "DateTicks";
+
+            long ticks;
+            long.TryParse(ConfigurationManager.AppSettings[keyNameTicks], out ticks);
+            DateTime dateTicks = new DateTime(ticks);
+
+            DateTime date;
+            DateTime.TryParse(ConfigurationManager.AppSettings[keyNameDate], out date);
+
+            int valueInt;
+            int.TryParse(ConfigurationManager.AppSettings[keyName], out valueInt);
+            valueInt++;
+
+            string file = System.Reflection.Assembly.GetEntryAssembly().Location;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(file);
+
+            DateTime now = DateTime.Now;
+            config.AppSettings.Settings.Add(keyNameTicks, now.Ticks.ToString());
+            config.AppSettings.Settings.Add(keyNameDate, now.ToString("O"));
+            config.AppSettings.Settings.Add(keyName, valueInt.ToString());
+
+            config.Save(ConfigurationSaveMode.Minimal);
+            ConfigurationManager.RefreshSection("appSettings");
+
         }
     }
 }
--- UnitTestsNet/UnitTest1.cs
+++ UnitTestsNet/UnitTest1.cs
@@ -8,15 +8,11 @@ namespace AspNetConsoleApp1
     {
         public TestContext TestContext { get; set; }
 
-        [TestInitialize]
-        public void TestInit()
-        {
-            TestContext.WriteLine("HELLO");
-        }
-
         [TestMethod]
         public void TestMethod1()
         {
+
+            TestContext.WriteLine("HELLO");
             System.Console.WriteLine("TEST");
             Model1 model1 = new Model1()
             {
@@ -24,7 +20,7 @@ namespace AspNetConsoleApp1
                 Name = "Josef"
             };
 
-            Assert.AreEqual("Josef", model1.Name);
+            Assert.AreEqual("Karel", model1.Name);
             Assert.IsTrue(model1.Id > 0);
             Assert.IsTrue(model1.Name.Length > 0);
         }
